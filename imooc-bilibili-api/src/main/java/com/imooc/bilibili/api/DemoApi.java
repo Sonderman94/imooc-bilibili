d048fbcbdb2db498e9b964acba69a53ac4e21547
Sonder_man <ZIEND_0904@163.com>
2025-05-05 12:05:29 +0800

    建立了存储数据的datamap，实现了Resful风格的http方法

--- imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/DemoApi.java
+++ imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/DemoApi.java
@@ -1,9 +1,12 @@
 package com.imooc.bilibili.api;
 
 import com.imooc.bilibili.service.DemoService;
+import com.sun.javafx.collections.MappingChange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class DemoApi {
